a5493cac8af2e45d5c662ca9b6057d1131b92e3c
xiaoqing <zhangxiaoqing0221@gmail.com>
2015-08-29 21:18:42 +0800

    050Pow

---
+++ 050Pow/src/Solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+	public double power(double x, int n) {
+		if (n == 0)
+			return 1;
+		double v = power(x, n / 2);
+		if (n % 2 == 0) {
+			return v * v;
+		} else {
+			return v * v * x;
+		}
+	}
+
+	public double pow(double x, int n) {
+		if (n < 0)
+			return 1.0 / power(x, -n);
+		else
+			return power(x, n);
+	}
+}
